561390faa60fd93e893644436047d2a84763d717
Estela Martínez Lozano <emarloz@upv.edu.es>
2023-10-03 18:22:53 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace PropAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Usuario")]
+    public class UsuarioController
+    {
+        [HttpGet]
+        public IEnumerable<dynamic> Get()
+        {
+            using (PropBDContext ctx = new PropBDContext())
+            {
+                var l = ctx.Usuario.Include(u => u.IdComercio).ToList();
+                return l;
+            }
+        }
+    }
+}
