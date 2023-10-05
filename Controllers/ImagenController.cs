7507192d62df005691d595f87beb05f74fe1e976
Estela Martínez Lozano <emarloz@upv.edu.es>
2023-10-05 10:54:13 +0200

    Get y post image sin sql server

--- Controllers/ImagenController.cs
+++ Controllers/ImagenController.cs
@@ -17,8 +17,46 @@ namespace PropAPI.Controllers
     [Route("api/Imagen")]
     public class ImagenController : ControllerBase
     {
+        [HttpGet("{nombreImagen}")]
+        public async Task<IActionResult> GetImagen(string nombreImagen)
+        {
+            try
+            {
+                // Crear un cliente BlobServiceClient
+                BlobServiceClient blobServiceClient = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=storageprop;AccountKey=9KA/FW5ozB1VLLg3ZorWfmcOd0qbzO3HLeVksK5PGyWcN8JUzWdVNJtU/Vb6DQT52tbASAcxJTy6+AStAtcc5g==;EndpointSuffix=core.windows.net");
+
+
+                // Nombre del contenedor donde se encuentra la imagen
+                string containerName = "imagenes"; // Reemplaza con el nombre de tu contenedor
+
+                // Crear un cliente BlobContainerClient
+                BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+                // Crear un cliente BlobClient para la imagen específica
+                BlobClient blobClient = containerClient.GetBlobClient(nombreImagen);
+
+                BlobDownloadInfo blobDownloadInfo = await blobClient.DownloadAsync();
+
+                using (Stream stream = blobDownloadInfo.Content)
+                {
+                    // Devolver la imagen como una respuesta HTTP
+                    if (stream != null)
+                    {
+                        return File(stream, blobDownloadInfo.ContentType);
+                    }
+                }
+
+                return NotFound(); // Si la imagen no se encuentra
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al descargar la imagen: {ex.Message}");
+                return StatusCode(500); // Maneja el error de manera adecuada en tu aplicación
+            }
+        }
+
         [HttpPost("{nombreImagen}")]
-        public async Task PostImage(string nombreImagen, [FromBody] string base64)
+        public void PostImage(string nombreImagen, [FromBody] string base64)
         {
             var base64Parts = base64.Split(',');
             string encodedData = base64Parts.Length > 1 ? base64Parts[1] : base64;
@@ -37,48 +75,11 @@ namespace PropAPI.Controllers
                     var blobClient = blobContainerClient.GetBlobClient(nombreImagen);
 
                     blobClient.Upload(streamImage, true);
-                    return;
                 } catch (Exception e)
                 {
 
                 }
-
             }
-
-
-            /* try
-             {
-                 // Crear un cliente BlobServiceClient
-                 BlobServiceClient blobServiceClient = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=storageprop;AccountKey=9KA/FW5ozB1VLLg3ZorWfmcOd0qbzO3HLeVksK5PGyWcN8JUzWdVNJtU/Vb6DQT52tbASAcxJTy6+AStAtcc5g==;EndpointSuffix=core.windows.net");
-
-
-                 // Nombre del contenedor donde se encuentra la imagen
-                 string containerName = "imagenes"; // Reemplaza con el nombre de tu contenedor
-
-                 // Crear un cliente BlobContainerClient
-                 BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-
-                 // Crear un cliente BlobClient para la imagen específica
-                 BlobClient blobClient = containerClient.GetBlobClient(nombreImagen);
-
-                 BlobDownloadInfo blobDownloadInfo = await blobClient.DownloadAsync();
-
-                 using (Stream stream = blobDownloadInfo.Content)
-                 {
-                     // Devolver la imagen como una respuesta HTTP
-                     if (stream != null)
-                     {
-                         return File(stream, blobDownloadInfo.ContentType);
-                     }
-                 }
-
-                 return NotFound(); // Si la imagen no se encuentra
-             }
-             catch (Exception ex)
-             {
-                 Console.WriteLine($"Error al descargar la imagen: {ex.Message}");
-                 return StatusCode(500); // Maneja el error de manera adecuada en tu aplicación
-             }*/
         }
     }
 }
