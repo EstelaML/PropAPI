21ed68fc96c935aa1db62237e79c53eb0244a23a
Andres Felder <andyfelder16@gmail.com>
2023-10-17 21:43:09 +0200

    new Program.cs

--- App/Program.cs
+++ App/Program.cs
@@ -1,40 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+// Configura Kestrel para escuchar en todas las interfaces de red
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5000); // Cambia 5000 al número de puerto que estás usando
+});
 
+// Configura servicios y middleware
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+});
 
+// Configura CORS para permitir cualquier origen
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(
-                      policy =>
-                      {
-                          policy.WithOrigins("http://localhost/");
-                      });
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+    });
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
-
 app.UseHttpsRedirection();
-
 app.UseCors();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-
