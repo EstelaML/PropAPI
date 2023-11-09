cc73140c8d6cf6c319dbd5ace52a32417f8d551b
Estela Martínez Lozano <emarloz@upv.edu.es>
2023-11-09 11:57:43 +0100

    fix reseña

--- App/Models/PropBDContext.cs
+++ App/Models/PropBDContext.cs
@@ -108,8 +108,7 @@ public partial class PropBDContext : DbContext
 
         modelBuilder.Entity<Reseña>(entity =>
         {
-            entity.HasKey(e => e.comercio).HasName("PK__Tipo__3214EC07FBE66B73");
-            entity.HasKey(e => e.usuario).HasName("PK__Tipo__3214EC07FBE66B76");
+            entity.HasKey(r => new { r.comercio, r.usuario});
 
             entity.Property(e => e.descripcion).HasMaxLength(200);
             entity.Property(e => e.titulo).HasMaxLength(100);
