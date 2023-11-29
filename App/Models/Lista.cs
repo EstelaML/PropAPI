46a6e904d7e34264c3cdc5d288388ec0c2164d6c
Juan Pablo Bueno Fuentes <JPBUEFUE@upv.edu.es>
2023-11-29 10:14:19 +0100

    seguir, dejar de seguir y ver listas seguidas

--- App/Models/Lista.cs
+++ App/Models/Lista.cs
@@ -5,8 +5,8 @@ public class Lista
     public int id { get; set; }
     public int idusuario { get; set; }
     public string nombre { get; set; }
-    public string decripcion { get; set; }
-    public virtual Usuario? usuario { get; set; }
+    public string? descripcion { get; set; }
+    public virtual Usuario usuario { get; set; }
     public virtual ICollection<Comercio> Comercio { get; set; } = new List<Comercio>();
 
     public virtual ICollection<Usuario> usuarioSeguidos { get; set; } = new List<Usuario>();
