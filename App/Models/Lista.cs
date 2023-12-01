4ebcd00388fb6150e079cba8a8cc558c70d7ec89
El Rubén <rubo23dingo@gmail.com>
2023-12-01 11:52:37 +0100

    a

--- App/Models/Lista.cs
+++ App/Models/Lista.cs
@@ -5,7 +5,7 @@ public class Lista
     public int id { get; set; }
     public int idusuario { get; set; }
     public string nombre { get; set; }
-    public string decripcion { get; set; }
+    public string descripcion { get; set; }
     public virtual Usuario? usuario { get; set; }
     public virtual ICollection<Comercio> Comercio { get; set; } = new List<Comercio>();
 }
